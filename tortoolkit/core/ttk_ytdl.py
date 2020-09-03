fdfd8f94e684343d1f19c860271284bdb913a687
yash-dk <yashkhadse@gmail.com>
2020-09-03 23:50:36 +0530

    ytdl patches #F1.3

--- tortoolkit/core/ttk_ytdl.py
+++ tortoolkit/core/ttk_ytdl.py
@@ -207,7 +207,7 @@ async def handle_ytdl_file_download(e: MessageLike, queue: asyncio.Queue):
     # ytdldfile | format_id | sender_id | suid | is_audio
     data = e.data.decode("UTF-8")
     data = data.split("|")
-    await e.edit(buttons=None)
+    
     
     if data[2] != str(e.sender_id):
         await e.answer("Not valid user, Dont touch.")
@@ -215,6 +215,8 @@ async def handle_ytdl_file_download(e: MessageLike, queue: asyncio.Queue):
     else:
         await e.answer("Crunching Data.....")
     
+    await e.edit(buttons=None)
+
     path = os.path.join(os.getcwd(),'userdata',data[3]+".json")
     if os.path.exists(path):
         with open(path,encoding="UTF-8") as file:
@@ -277,18 +279,18 @@ async def handle_ytdl_playlist(e: MessageLike) -> None:
         entlen = len(entities)
         keybr = list()
         
-        # format> ytdlplaylist | quality | suid
+        # format> ytdlplaylist | quality | suid | sender_id
         suid = str(time.time()).replace(".","")
 
         for i in ["144","240","360","480","720","1080","1440","2160"]:
-            keybr.append([KeyboardButtonCallback(text=f"{i}p All videos",data=f"ytdlplaylist|{i}|{suid}")])
+            keybr.append([KeyboardButtonCallback(text=f"{i}p All videos",data=f"ytdlplaylist|{i}|{suid}|{e.sender_id}")])
 
-        keybr.append([KeyboardButtonCallback(text=f"Best All videos",data=f"ytdlplaylist|best|{suid}")])
+        keybr.append([KeyboardButtonCallback(text=f"Best All videos",data=f"ytdlplaylist|best|{suid}|{e.sender_id}")])
         
         
-        keybr.append([KeyboardButtonCallback(text="Best all audio only. [340k]",data=f"ytdlplaylist|320k|{suid}")])
-        keybr.append([KeyboardButtonCallback(text="Medium all audio only. [128k]",data=f"ytdlplaylist|128k|{suid}")])
-        keybr.append([KeyboardButtonCallback(text="Worst all audio only. [64k]",data=f"ytdlplaylist|64k|{suid}")])
+        keybr.append([KeyboardButtonCallback(text="Best all audio only. [340k]",data=f"ytdlplaylist|320k|{suid}|{e.sender_id}")])
+        keybr.append([KeyboardButtonCallback(text="Medium all audio only. [128k]",data=f"ytdlplaylist|128k|{suid}|{e.sender_id}")])
+        keybr.append([KeyboardButtonCallback(text="Worst all audio only. [64k]",data=f"ytdlplaylist|64k|{suid}|{e.sender_id}")])
 
         await msg.edit(f"Found {entlen} videos in the playlist.",buttons=keybr) 
 
@@ -307,8 +309,15 @@ async def handle_ytdl_playlist(e: MessageLike) -> None:
         torlog.exception("Playlist Parse failed") 
 
 async def handle_ytdl_playlist_down(e: MessageLike, queue: asyncio.Queue) -> None:
-    # ytdlplaylist | quality | suid
+    # ytdlplaylist | quality | suid | sender_id
     data = e.data.decode("UTF-8").split("|")
+    
+    if data[3] != str(e.sender_id):
+        await e.answer("Not valid user, Dont touch.")
+        return
+    else:
+        await e.answer("Crunching Data.....")
+
     await e.edit(buttons=None)
     path = os.path.join(os.getcwd(),"userdata",data[2]+".json")
     if os.path.exists(path):
@@ -329,7 +338,10 @@ async def handle_ytdl_playlist_down(e: MessageLike, queue: asyncio.Queue) -> Non
             else:
                 await upload_handel(opdir, await e.get_message(), e.sender_id, dict(), queue=queue)
         else:
-            vidcmd = f"youtube-dl --continue --embed-subs --no-warnings --prefer-ffmpeg -f 'bestvideo[ext=mp4,height<={data[1]}]+bestaudio[ext=m4a]/best' -o '{opdir}/%(playlist_index)s - %(title)s.%(ext)s' {url}"
+            if data[1] == "best":
+                vidcmd = f"youtube-dl --continue --embed-subs --no-warnings --prefer-ffmpeg -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best' -o '{opdir}/%(playlist_index)s - %(title)s.%(ext)s' {url}"
+            else:
+                vidcmd = f"youtube-dl --continue --embed-subs --no-warnings --prefer-ffmpeg -f 'bestvideo[ext=mp4,height<={data[1]}]+bestaudio[ext=m4a]/best' -o '{opdir}/%(playlist_index)s - %(title)s.%(ext)s' {url}"
             out, err = await cli_call(vidcmd)
             if err:
                 await e.reply(f"Failed to download the videos <code>{err}</code>",parse_mode="html")
