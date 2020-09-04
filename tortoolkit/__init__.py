5e4522202c0b9bb1c612026b67e0a0a0d5eb94f7
yash-dk <yashkhadse@gmail.com>
2020-09-05 01:09:27 +0530

    file handler

--- tortoolkit/__init__.py
+++ tortoolkit/__init__.py
@@ -6,8 +6,10 @@ __author__ = "YashDK Github@yash-dk"
 import logging
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s %(levelname)s %(threadName)s %(name)s %(message)s"
+    format="%(asctime)s %(levelname)s %(threadName)s %(name)s %(message)s",
+    handlers=[logging.StreamHandler(),logging.FileHandler("torlog.txt")]
 )
+
 from tortoolkit.core.server import start_server
 from .core.database_handle import TtkUpload,TorToolkitDB
 logging.info("Database created")
