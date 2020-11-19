21586b4693c942c701e57905aa3a2fe55283775b
yash-dk <yashkhadse@gmail.com>
2020-11-20 01:23:43 +0530

    rmpth

--- tortoolkit/core/HandleManager.py
+++ tortoolkit/core/HandleManager.py
@@ -268,7 +268,7 @@ async def handle_test_command(e):
     db = TtkUpload()
     msg = await e.reply("test")
     msg = await e.client.get_messages(e.chat_id,ids=msg.id)
-    await rclone_driver("/mnt/d/GitMajors/yolo.mkv",msg)
+    await rclone_driver("",msg)
     del db
 
 
