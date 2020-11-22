7545dcee82654fcd9f127c8742c4cefbd1ba0780
yash-dk <yashkhadse@gmail.com>
2020-11-22 14:07:37 +0530

    added viewposts

--- tortoolkit/core/server.py
+++ tortoolkit/core/server.py
@@ -15,6 +15,7 @@ page = """
 <html>
 <head>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
 body {
   padding: 20px;
