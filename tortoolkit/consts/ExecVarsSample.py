e2d8c94d51144d3cae74f774e91efe241444fcf3
X_ROW࿐™ <59341122+sahadz@users.noreply.github.com>
2021-01-27 18:54:29 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -13,7 +13,7 @@ except:
         ALD_USR = set(int(x) for x in os.environ.get("AUTH_CHANNEL", "").split())
         
         # Google Drive Index Link should include the base dir also See readme for more info
-        GD_INDEX_URL = False
+        GD_INDEX_URL = os.environ.get("GD_INDEX_URL", "false")
 
         # Time to wait before edit message
         EDIT_SLEEP_SECS = int(os.environ.get("EDIT_SLEEP_TIME_OUT", 15))
@@ -22,7 +22,7 @@ except:
         TG_UP_LIMIT = 1700000000
 
         # Should force evething uploaded into Document
-        FORCE_DOCUMENTS = False
+        FORCE_DOCUMENTS = os.environ.get("FORCE_DOCUMENTS", "false")
 
         # Chracter to use as a completed progress 
         COMPLETED_STR = os.environ.get("COMPLETED_STR", "█")
@@ -31,7 +31,7 @@ except:
         REMAINING_STR = os.environ.get("REMAINING_STR", "░")
 
         # DB URI for access
-        DB_URI = "dbname=tortk user=postgres password=your-pass host=127.0.0.1 port=5432"
+        DB_URI = os.environ.get("DATABASE_URL", "")
         
         # UNCOMMENT THE BELOW LINE WHEN USING CONTAINER AND COMMENT THE UPPER LINE
         #DB_URI = "dbname=tortk user=postgres password=your-pass host=db port=5432"
@@ -51,16 +51,16 @@ except:
         DEFAULT_TIMEOUT = "leech"
 
         # For vps set path here or you can use runtime too
-        RCLONE_CONFIG = False
+        RCLONE_CONFIG = os.environ.get("RCLONE_CONFIG", "")
         
         # Name of the RCLONE drive from the config
-        DEF_RCLONE_DRIVE = ""
+        DEF_RCLONE_DRIVE = os.environ.get("DEF_RCLONE_DRIVE", "")
 
         # Max size of a playlist that is allowed (Number of videos)
-        MAX_YTPLAYLIST_SIZE = 20
+        MAX_YTPLAYLIST_SIZE = int(os.environ.get("MAX_YTPLAYLIST_SIZE", "20"))
         
         # Max size of the torrent allowed
-        MAX_TORRENT_SIZE = 10
+        MAX_TORRENT_SIZE = int(os.environ.get("MAX_TORRENT_SIZE", "10"))
 
         # This is to stop someone from abusing the system by imposing the limit
         # [<GBs of total torrent sapce>, <Number of youtube videos allowed to download>, <Number of youtube playlists allowed to download>]
