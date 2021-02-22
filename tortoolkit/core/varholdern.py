1aa7ccffddffc9962e499ca35b612a22bc2994cc
yash-dk <yashkhadse@gmail.com>
2021-02-23 02:02:16 +0530

    Fixed ENV Vars Type casting

--- tortoolkit/core/varholdern.py
+++ tortoolkit/core/varholdern.py
@@ -37,6 +37,9 @@ class VarHolder:
         #Get the variable form the env [overlap]
         #try:
         envval = os.environ.get(variable)
+        INTS = ["EDIT_SLEEP_SECS", "MAX_TORRENT_SIZE", "MAX_YTPLAYLIST_SIZE", "TG_UP_LIMIT", "API_ID"]
+        BOOLS = ["FORCE_DOCUMENTS", "LEECH_ENABLED", "RCLONE_ENABLED"]
+
         if variable == "ALD_USR":
             if envval is not None:
                 templi = envval.split(" ")
@@ -51,7 +54,16 @@ class VarHolder:
                     val.extend(templi2)
                 else:
                     val = templi
-            
+        elif variable in INTS:
+            val =  int(envval) if envval is not None else val
+        elif variable in BOOLS:
+            if envval:
+                if "true" in envval.lower():
+                    val = True
+                else:
+                    val = False
+            else:
+                val = None
         else:
             val =  envval if envval is not None else val
 
