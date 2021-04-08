1afda4502cb7f4610a65e72492a3f9263a2ef247
yash-dk <yashkhadse@gmail.com>
2021-04-08 19:37:53 +0530

    aria2p got async

--- tortoolkit/functions/ariatools.py
+++ tortoolkit/functions/ariatools.py
@@ -6,10 +6,12 @@ from ..core.getVars import get_val
 from telethon.tl.types import KeyboardButtonCallback
 from telethon.errors.rpcerrorlist import MessageNotModifiedError
 from ..core.status.status import ARTask
+from functools import partial
 
 # referenced from public leech
 # pylint: disable=no-value-for-parameter
 torlog = logging.getLogger(__name__)
+aloop = asyncio.get_event_loop()
 
 async def aria_start():
     aria2_daemon_start_cmd = []
@@ -40,38 +42,29 @@ async def aria_start():
     stdout, stderr = await process.communicate()
     torlog.debug(stdout)
     torlog.debug(stderr)
-    aria2 = aria2p.API(
-        aria2p.Client(
-            host="http://localhost",
-            port=8100,
-            secret=""
-        )
-    )
+    arcli = await aloop.run_in_executor(None, partial(aria2p.Client, host="http://localhost", port=8100, secret=""))
+    aria2 = await aloop.run_in_executor(None, aria2p.API, arcli)
+
     return aria2
 
-def add_magnet(aria_instance, magnetic_link, c_file_name):
+async def add_magnet(aria_instance, magnetic_link, c_file_name):
     try:
-        download = aria_instance.add_magnet(
-            magnetic_link
-        )
+        download = await aloop.run_in_executor(None, aria_instance.add_magnet, magnetic_link)
     except Exception as e:
         return False, "**FAILED** \n" + str(e) + " \nPlease do not send SLOW links. Read /help"
     else:
         return True, "" + download.gid + ""
 
 
-def add_torrent(aria_instance, torrent_file_path):
+async def add_torrent(aria_instance, torrent_file_path):
     if torrent_file_path is None:
         return False, "**FAILED** \n\nsomething wrongings when trying to add <u>TORRENT</u> file"
     if os.path.exists(torrent_file_path):
         # Add Torrent Into Queue
         try:
-            download = aria_instance.add_torrent(
-                torrent_file_path,
-                uris=None,
-                options=None,
-                position=None
-            )
+
+            download = await aloop.run_in_executor(None, partial(aria_instance.add_torrent, torrent_file_path, uris=None, options=None, position=None))
+
         except Exception as e:
             return False, "**FAILED** \n" + str(e) + " \nPlease do not send SLOW links. Read /help"
         else:
@@ -80,20 +73,20 @@ def add_torrent(aria_instance, torrent_file_path):
         return False, "**FAILED** \n" + str(e) + " \nPlease try other sources to get workable link"
 
 
-def add_url(aria_instance, text_url, c_file_name):
+async def add_url(aria_instance, text_url, c_file_name):
     uris = [text_url]
     # Add URL Into Queue
     try:
-        download = aria_instance.add_uris(
-            uris
-        )
+        
+        download = await aloop.run_in_executor(None, aria_instance.add_uris, uris)
+
     except Exception as e:
         return False, "**FAILED** \n" + str(e) + " \nPlease do not send SLOW links. Read /help"
     else:
         return True, "" + download.gid + ""
 
 async def check_metadata(aria2, gid):
-    file = aria2.get_download(gid)
+    file = await aloop.run_in_executor(None, aria2.get_download, gid)
     torlog.info(file)
     if not file.followed_by_ids:
         return None
@@ -113,14 +106,14 @@ async def aria_dl(
     await ar_task.set_original_mess()
 
     if incoming_link.lower().startswith("magnet:"):
-        sagtus, err_message = add_magnet(aria_instance, incoming_link, c_file_name)
+        sagtus, err_message = await add_magnet(aria_instance, incoming_link, c_file_name)
     elif incoming_link.lower().endswith(".torrent"):
-        #sagtus, err_message = add_torrent(aria_instance, incoming_link)
-        #sagtus, err_message = add_url(aria_instance, incoming_link, c_file_name)
+        #sagtus, err_message = await add_torrent(aria_instance, incoming_link)
+        #sagtus, err_message = await add_url(aria_instance, incoming_link, c_file_name)
         await ar_task.set_inactive("Cant download this .torrent file")
         return False, ar_task
     else:
-        sagtus, err_message = add_url(aria_instance, incoming_link, c_file_name)
+        sagtus, err_message = await add_url(aria_instance, incoming_link, c_file_name)
     if not sagtus:
         await ar_task.set_inactive(err_message)
         return sagtus, ar_task
@@ -162,7 +155,7 @@ async def aria_dl(
     else:
         statusr, stmsg = op
         if statusr:
-            file = aria_instance.get_download(err_message)
+            file = await aloop.run_in_executor(None, aria_instance.get_download,err_message)
             to_upload_file = file.name
             await ar_task.set_path(to_upload_file)
             await ar_task.set_done()
@@ -173,7 +166,7 @@ async def aria_dl(
 
 async def check_progress_for_dl(aria2, gid, event, previous_message, task, rdepth = 0, user_msg=None):
     try:
-        file = aria2.get_download(gid)
+        file = await aloop.run_in_executor(None, aria2.get_download, gid)
         complete = file.is_complete
         if not complete:
             if not file.error_message:
@@ -232,7 +225,7 @@ async def check_progress_for_dl(aria2, gid, event, previous_message, task, rdept
 async def remove_dl(gid):
     aria2 = await aria_start()
     try:
-        downloads = aria2.get_download(gid)
+        downloads = await aloop.run_in_executor(None, aria2.get_download, gid)
         downloads.remove(force=True, files=True)
     except:
         torlog.exception("exc")
