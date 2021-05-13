bd60f537f1844cbb05305a2d554337b3a60debab
FA Tulloh <77918734+yourtulloh@users.noreply.github.com>
2021-05-14 01:52:56 +0800

    Fix zippy & add racaty.net

--- tortoolkit/functions/dl_generator.py
+++ tortoolkit/functions/dl_generator.py
@@ -1,6 +1,7 @@
 import re
 import aiohttp
 import math
+import urllib.parse
 import logging
 from bs4 import BeautifulSoup
 
@@ -20,45 +21,54 @@ async def generate_directs(url):
             ourl = info.get('href')
             return ourl
         except:
-            return "**ERROR No mediafire link found.**"
+            return "**ERROR No mediafire link found.**"      
         
+    #zippyshare.com fix revert back to old. Based on https://github.com/LameLemon/ziggy
+    elif 'zippyshare.com' in url:
+        try:
+            link = re.findall(r'\bhttps?://.*zippyshare\.com\S+', url)[0]
+            async with aiohttp.ClientSession() as ttksess:
+                resp = await ttksess.get(link)
+                restext = await resp.text()
+                
+            base_url = re.search('http.+.com', restext).group()
+            page_soup = BeautifulSoup(restext, 'lxml')
+            scripts = page_soup.find_all("script", {"type": "text/javascript"})
+            for script in scripts:
+                if "getElementById('dlbutton')" in script.text:
+                    url_raw = re.search(r'= (?P<url>\".+\" \+ (?P<math>\(.+\)) .+);',
+                                        script.text).group('url')
+                    math = re.search(r'= (?P<url>\".+\" \+ (?P<math>\(.+\)) .+);',
+                       script.text).group('math')
+                    url = url_raw.replace(math, '"' + str(eval(math)) + '"')
+                    break
+            ourl = base_url + eval(url)
+            name = urllib.parse.unquote(url.split('/')[-1])
+            return ourl
+        except:
+            return "**ERROR: No zippyshare link found.**"
         
-    #Zippyshare
-    elif  'zippyshare.com' in url:
-        
-        #Zippyshare fix directlink generator for ttk by @yourtulloh based on:
-        #Zippyshare up-to-date plugin from https://github.com/UsergeTeam/Userge-Plugins/blob/master/plugins/zippyshare.py
-        #Thanks to all contributors @aryanvikash, rking32, @BianSepang
-        tulloh = r'https://www(\d{1,3}).zippyshare.com/v/(\w{8})/file.html'
-        regex_result = (
-            r'var a = (\d{6});\s+var b = (\d{6});\s+document\.getElementById'
-            r'\(\'dlbutton\'\).omg = "f";\s+if \(document.getElementById\(\''
-            r'dlbutton\'\).omg != \'f\'\) {\s+a = Math.ceil\(a/3\);\s+} else'
-            r' {\s+a = Math.floor\(a/3\);\s+}\s+document.getElementById\(\'d'
-            r'lbutton\'\).href = "/d/[a-zA-Z\d]{8}/\"\+\(a \+ \d{6}%b\)\+"/('
-            r'[\w%-.]+)";'
-        )
-        
-        match = re.match(tulloh, url)
-        if not match:
-            return "**ERROR Invalid Zippyshare URL.**"
-        
-        server, id_ = match.group(1), match.group(2)
-        async with aiohttp.ClientSession() as ttksess:
-            resp = await ttksess.get(url)
-            restext = await resp.text()
-            
-        match = re.search(regex_result, restext, re.DOTALL)
-        if not match:
-            return "**ERROR Invalid response, try again!**"
-        
-        val_1 = int(match.group(1))
-        val_2 = math.floor(val_1 / 3)
-        val_3 = int(match.group(2))
-        val = val_1 + val_2 % val_3
-        name = match.group(3)
-        ourl = "https://www{}.zippyshare.com/d/{}/{}/{}".format(server, id_, val, name)
-        return ourl
+    #racaty.net
+    elif 'racaty.net' in url:
+        try:
+            link = re.findall(r'\bhttps?://.*racaty\.net\S+', url)[0]
+            async with aiohttp.ClientSession() as ttksess:
+                resp = await ttksess.get(link)
+                restext = await resp.text()
+                
+            bss=BeautifulSoup(restext,'html.parser')
+            op=bss.find('input',{'name':'op'})['value']
+            id=bss.find('input',{'name':'id'})['value']
+                  
+            async with aiohttp.ClientSession() as ttksess:
+                rep = await ttksess.post(link,data={'op':op,'id':id})
+                reptext = await rep.text()
+                
+            bss2=BeautifulSoup(reptext,'html.parser')
+            ourl = bss2.find('a',{'id':'uniqueExpirylink'})['href']
+            return ourl
+        except:
+            return "**ERROR: No racaty link found.**"
         
     elif 'yadi.sk' in url or 'disk.yandex.com' in url:
         try:
