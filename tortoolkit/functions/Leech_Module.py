6a97290ff53f579d888a20e90537590628a8dcdd
Weeb <72728580+WeebTime@users.noreply.github.com>
2021-06-04 23:31:59 +0530

    Update Leech_Module.py

--- tortoolkit/functions/Leech_Module.py
+++ tortoolkit/functions/Leech_Module.py
@@ -471,24 +471,24 @@ async def handle_ext_zip(path, rmess, omess):
             return ext_path
             
 async def errored_message(e, reason):
-    msg = f"<a href='tg://user?id={e.sender_id}'>Done</a>\nYour Download Failed."
+    msg = f"<a href='tg://user?id={e.sender_id}'>Done</a>\n\nYour Download Failed."
     if reason is not None:
         await reason.reply(msg, parse_mode="html")
     else:
         await e.reply(msg, parse_mode="html")
 
 async def print_files(e,files,thash=None, path = None, size=None):
-    msg = f"<a href='tg://user?id={e.sender_id}'>Done</a>\n#uploads\n"
+    msg = f"<a href='tg://user?id={e.sender_id}'>Done</a>\n#Uploads\n"
 
     if path is not None and size is None:
         size = calculate_size(path)
         transfer[0] += size
         size = human_readable_bytes(size)
-        msg += f"Uploaded Size:- {str(size)}\n\n"
+        msg += f"Uploaded Size: {str(size)}\n\n"
     elif size is not None:
         transfer[0] += size
         size = human_readable_bytes(size)
-        msg += f"Uploaded Size:- {str(size)}\n\n"
+        msg += f"Uploaded Size: {str(size)}\n\n"
 
 
     if len(files) == 0:
@@ -498,11 +498,11 @@ async def print_files(e,files,thash=None, path = None, size=None):
     msg_li = []
     for i in files.keys():
         link = f'https://t.me/c/{str(chat_id)[4:]}/{files[i]}'
-        if len(msg + f'🚩 <a href="{link}">{i}</a>\n') > 4000:
+        if len(msg + f'→ <a href="{link}">{i}</a>\n') > 4000:
             msg_li.append(msg)
-            msg = f'🚩 <a href="{link}">{i}</a>\n'
+            msg = f'→ <a href="{link}">{i}</a>\n'
         else:
-            msg += f'🚩 <a href="{link}">{i}</a>\n'
+            msg += f'→ <a href="{link}">{i}</a>\n'
 
     for i in msg_li:
         await e.reply(i,parse_mode="html")
