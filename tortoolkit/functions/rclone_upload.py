2473f4ece556bac080b6b2ea84061efa6084d36d
Amirul Andalib <amirulandalib@gmail.com>
2021-07-25 22:03:33 +0600

    fix Rclone folder upload count
    fix Rclone folder upload count

--- tortoolkit/functions/rclone_upload.py
+++ tortoolkit/functions/rclone_upload.py
@@ -369,4 +369,14 @@ def calculate_size(path):
         return 0
 
 
+def get_size_fl(start_path = '.'):
+    total_size = 0
+    for dirpath, _, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+
+    return total_size
 # probably hotfix for rclone ban
