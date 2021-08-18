180a1468d8d8a73180466061727f5b8cd9778194
Amirul Islam <amirulandalib@gmail.com>
2021-08-18 16:32:51 +0600

    massive changes to file selection ui
    thanks to @bipuldey19-Telegram and Slam Mirror Bot for this Mod.....

--- tortoolkit/core/wserver.py
+++ tortoolkit/core/wserver.py
@@ -3,6 +3,11 @@
 # (c) modified by AmirulAndalib [amirulandalib@github]
 
 import asyncio
+
+import os
+
+import time
+
 import logging
 
 import qbittorrentapi as qba
@@ -16,34 +21,245 @@ torlog = logging.getLogger(__name__)
 routes = web.RouteTableDef()
 
 page = """
-<html>
-<head>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Torrent File Selector</title>
+    <link rel="icon" href="https://telegra.ph/file/69e1118355aac6913ab1b.jpg" type="image/jpg">
+    <script
+      src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+      integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs="
+      crossorigin="anonymous"
+    ></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+      integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+      crossorigin="anonymous"
+    />
 <style>
-body {
-  padding: 20px;
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Ubuntu", sans-serif;
+    list-style: none;
+    text-decoration: none;
+    outline: none !important;
+    color: white;
 }
-ul { 
-  list-style: none;
-  margin: 5px 20px;
+body{
+    background-color: #0D1117;
 }
-li {
-  margin: 10px 0;
+header{
+    margin: 3vh 1vw;
+    padding: 0.5rem 1rem 0.5rem 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: #161B22;
+    border-radius: 30px;
+    background-color: #161B22;
+    border: 2px solid rgba(255, 255, 255, 0.11);
 }
 
+header:hover, section:hover{
+    box-shadow: 0px 0px 15px black;
+}
 
-p { font-size: 12px; margin: 24px;}
+.brand{
+    display: flex;
+    align-items: center;
+}
+
+img{ 
+    width: 2.5rem;
+    height: 2.5rem;
+    border: 2px solid black;
+    border-radius: 50%;
+}
+
+.name{
+    margin-left: 1vw;
+    font-size: 1.5rem;
+}
+
+.intro{
+    text-align: center;
+    margin-bottom: 2vh;
+    margin-top: 1vh;
+}
+
+.social a{
+    font-size: 1.5rem;
+    padding-left: 1vw;
+}
+
+.social a:hover, .brand:hover{
+    filter: invert(0.3);
+}
+
+section{
+    margin: 0vh 1vw;
+    margin-bottom: 10vh;
+    padding: 1vh 3vw;
+    display: flex;
+    flex-direction: column;
+    border: 2px solid rgba(255, 255, 255, 0.11);
+    border-radius: 20px;
+    background-color: #161B22 ;
+}
+
+li:nth-child(1){
+    padding: 1rem 1rem 0.5rem 1rem;
+}
+
+li:nth-child(n+1){
+    padding-left: 1rem;
+}
+
+li label{
+    padding-left: 0.5rem;
+}
+
+li{
+    padding-bottom: 0.5rem;
+}
+
+span{
+    margin-right: 0.5rem;
+    cursor: pointer;
+    user-select: none;
+    transition: transform 200ms ease-out;
+}
+
+span.active{
+    transform: rotate(90deg);
+    -ms-transform: rotate(90deg);	 /* for IE  */
+    -webkit-transform: rotate(90deg);/* for browsers supporting webkit (such as chrome, firefox, safari etc.). */
+    display: inline-block;
+}
+
+ul{
+    margin: 1vh 1vw 1vh 1vw;
+    padding: 0 0 0.5rem 0;
+    border: 2px solid black;
+    border-radius: 20px;
+    background-color: #1c2129;
+    overflow: hidden;
+}
+
+input[type="checkbox"]{
+    cursor: pointer;
+    user-select: none;
+}
+
+input[type="submit"] {
+    border-radius: 20px;
+    margin: 2vh auto 1vh auto;
+    width: 50%;
+    display: block;
+    height: 5.5vh;
+    border: 2px solid rgba(255, 255, 255, 0.11);
+    background-color: #0D1117;
+    font-size: 16px;
+    font-weight: 500;
+}
+
+input[type="submit"]:hover, input[type="submit"]:focus{
+    background-color: rgba(255, 255, 255, 0.068);
+    cursor: pointer;
+}
+
+@media (max-width: 768px){
+    input[type="submit"]{
+        width: 100%;
+    }
+}
+  
+#treeview .parent {
+    position: relative;
+}
+  
+#treeview .parent > ul {
+    display: none;
+}
+
 </style>
 </head>
 <body>
-<h1>👇Select the Files that you want to Be downloaded from Below 👇</h1>
-<form action="{form_url}" method="POST">
+  <!--© Designed and coded by @bipuldey19-Telegram-->
+    <header>
+      <div class="brand">
+        <img
+          src="https://telegra.ph/file/69e1118355aac6913ab1b.jpg"
+          alt="logo"
+        />
+        <a href="https://t.me/Xcodershub"> 
+          <h2 class="name">TorToolkitX</h2>
+        </a>
+      </div>
+      <div class="social">
+        <a href="https://github.com/Xcodershub/TortoolkitX"><i class="fab fa-github"></i></a>
+        <a href="https://t.me/Xcodershub"><i class="fab fa-telegram"></i></a>
+      </div>
+    </header>
+    <section>
+      <h2 class="intro">Select the files you want to download</h2>
+      <form action="{form_url}" method="POST">
+       {My_content}
+       <input type="submit" name="Select these files ;)">
+      </form>
+    </section>
 
-{My_content}
+    <script>
+      $(document).ready(function () {
+        var tags = $("li").filter(function () {
+          return $(this).find("ul").length !== 0;
+        });
 
-<input type="submit" name="Select these files ;)">
-</form>
+        tags.each(function () {
+          $(this).addClass("parent");
+        });
+
+        $("body").find("ul:first-child").attr("id", "treeview");
+        $(".parent").prepend("<span>▶</span>");
+
+        $("span").click(function (e) {
+          e.stopPropagation();
+          e.stopImmediatePropagation();
+          $(this).parent( ".parent" ).find(">ul").toggle("slow");
+          if ($(this).hasClass("active")) $(this).removeClass("active");
+          else $(this).addClass("active");
+        });
+      });
+
+      var labels = document.querySelectorAll("label");
+      //Shorting the file/folder names
+      labels.forEach(function (label) {
+        if (label.innerText.toString().split(" ").length != 4) {
+          let FirstPart = label.innerText
+            .toString()
+            .split(" ")
+            .slice(0, 2)
+            .join(" ");
+          let SecondPart = label.innerText
+            .toString()
+            .split(" ")
+            .splice(-3)
+            .join(" ");
+          label.innerText = `${FirstPart}... ${SecondPart}`;
+        }
+      });
+    </script>
 
 <script>
 $('input[type="checkbox"]').change(function(e) {
@@ -106,25 +322,236 @@ $('input[type="checkbox"]').change(function(e) {
 """
 
 code_page = """
-<html>
-<head>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-<title>
-TorToolKitX Torrent Files
-</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Torrent Code Checker</title>
+    <link rel="icon" href="https://telegra.ph/file/69e1118355aac6913ab1b.jpg" type="image/jpg"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+      integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+      crossorigin="anonymous"
+    />
+    <style>
+     *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Ubuntu", sans-serif;
+    list-style: none;
+    text-decoration: none;
+    color: white;
+}
+
+body{
+    background-color: #0D1117;
+}
+
+header{
+    margin: 3vh 1vw;
+    padding: 0.5rem 1rem 0.5rem 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: #161B22;
+    border-radius: 30px;
+    background-color: #161B22;
+    border: 2px solid rgba(255, 255, 255, 0.11);
+}
+
+header:hover, section:hover{
+    box-shadow: 0px 0px 15px black;
+}
+
+.brand{
+    display: flex;
+    align-items: center;
+}
+
+img{ 
+    width: 2.5rem;
+    height: 2.5rem;
+    border: 2px solid black;
+    border-radius: 50%;
+}
+
+.name{
+    color: white;
+    margin-left: 1vw;
+    font-size: 1.5rem;
+}
+
+.intro{
+    text-align: center;
+    margin-bottom: 2vh;
+    margin-top: 1vh;
+}
+
+.social a{
+    font-size: 1.5rem;
+    color: white;
+    padding-left: 1vw;
+}
+
+.social a:hover, .brand:hover{
+    filter: invert(0.3);
+}
+
+section{
+    margin: 0vh 1vw;
+    margin-bottom: 10vh;
+    padding: 1vh 3vw;
+    display: flex;
+    flex-direction: column;
+    border: 2px solid rgba(255, 255, 255, 0.11);
+    border-radius: 20px;
+    background-color: #161B22 ;
+    color: white;
+}
+
+section form{
+    display: flex;
+    margin-left: auto;
+    margin-right: auto;
+    flex-direction: column;
+}
+
+section div{
+    background-color: #0D1117;
+    border-radius: 20px;
+    max-width: fit-content;
+    padding: 0.7rem;
+    margin-top: 2vh;
+}
+
+section label{
+    font-size: larger;
+    font-weight: 500;
+    margin: 0 0 0.5vh 1.5vw;
+    display: block;
+}
+
+section input[type="text"]{
+    border-radius: 20px;
+    outline: none;
+    width: 50vw;
+    height: 4vh;
+    padding: 1rem;
+    margin: 0.5vh;
+    border: 2px solid rgba(255, 255, 255, 0.11);
+    background-color: #3e475531;
+    box-shadow: inset 0px 0px 10px black;
+}
+
+section input[type="text"]:focus{
+    border-color: rgba(255, 255, 255, 0.404);
+}
+
+section button{
+    border-radius: 20px;
+    margin-top: 1vh;
+    width: 100%;
+    height: 5.5vh;
+    border: 2px solid rgba(255, 255, 255, 0.11);
+    background-color: #0D1117;
+    color: white;
+    font-size: 16px;
+    font-weight: 500;
+    cursor: pointer;
+    transition: background-color 200ms ease;
+}
+
+section button:hover, section button:focus{
+    background-color: rgba(255, 255, 255, 0.068);
+}
+
+section span{
+    display: block;
+    font-size: x-small;
+    margin: 1vh;
+    font-weight: 100;
+    font-style: italic;
+    margin-left: 23%;
+    margin-right: auto;
+    margin-bottom: 2vh;
+}
+
+@media (max-width: 768px) {
+    section form{
+        flex-direction: column;
+        width: 90vw;
+    }
+
+    section div{
+        max-width: 100%;
+        margin-bottom: 1vh;
+    }
+
+    section label{
+        margin-left: 3vw;
+        margin-top: 1vh;
+    }
+
+    section input[type="text"]{
+        width: calc(100% - 0.3rem);
+    }
+
+    section button{
+        width: 100%;
+        height: 5vh;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    section span{
+        margin-left: 5%;
+    }
+}
+    </style>
+  </head>
 <body>
-<div class="container">
-<form action="{form_url}">
-  <div class="form-group">
-    <label for="pin_code">Pin Code</label>
-    <input type="text" class="form-control" name="pin_code" placeholder="Enter code to access the torrent">
-    <small class="form-text text-muted">Dont mess around. You download will get messed up.</small>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-</div>
+   <!--© Designed and coded by @bipuldey19-Telegram-->
+    <header>
+      <div class="brand">
+        <img
+          src="https://telegra.ph/file/69e1118355aac6913ab1b.jpg"
+          alt="logo"
+        />
+        <a href="https://t.me/Xcodershub">
+          <h2 class="name">TorToolkitX</h2>
+        </a>
+      </div>
+      <div class="social">
+        <a href="https://github.com/https://github.com/XcodersHub/TorToolkitX"><i class="fab fa-github"></i></a>
+        <a href="https://t.me/Xcodershub"><i class="fab fa-telegram"></i></a>
+      </div>
+    </header>
+    <section>
+      <form action="{form_url}">
+        <div>
+          <label for="pin_code">Pin Code :</label>
+          <input
+            type="text"
+            name="pin_code"
+            placeholder="Enter the code that you have got from Telegram to access the Torrent"
+          />
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+          <span
+            >* Dont mess around. Your download will get messed up.</
+          >
+    </section>
 </body>
 </html>
 """
@@ -141,9 +568,8 @@ async def list_torrent_contents(request):
         rend_page = code_page.replace("{form_url}", f"/tortk/files/{torr}")
         return web.Response(text=rend_page, content_type="text/html")
 
-    client = qba.Client(
-        host="localhost", port="8090", username="admin", password="adminadmin"
-    )
+    client = qba.Client(host="localhost", port="8090",
+                        username="admin", password="adminadmin")
     client.auth_log_in()
     try:
         res = client.torrents_files(torrent_hash=torr)
@@ -166,8 +592,7 @@ async def list_torrent_contents(request):
 
     rend_page = page.replace("{My_content}", cont[0])
     rend_page = rend_page.replace(
-        "{form_url}", f"/tortk/files/{torr}?pin_code={pincode}"
-    )
+        "{form_url}", f"/tortk/files/{torr}?pin_code={pincode}")
     client.auth_log_out()
     return web.Response(text=rend_page, content_type="text/html")
 
@@ -206,27 +631,24 @@ async def re_verfiy(paused, resumed, client, torr):
             torlog.info("Reverification Failed :- correcting stuff")
             # reconnect and issue the request again
             client.auth_log_out()
-            client = qba.Client(
-                host="localhost", port="8090", username="admin", password="adminadmin"
-            )
+            client = qba.Client(host="localhost", port="8090",
+                               username="admin", password="adminadmin")
             client.auth_log_in()
             try:
                 client.torrents_file_priority(
-                    torrent_hash=torr, file_ids=paused, priority=0
-                )
+                    torrent_hash=torr, file_ids=paused, priority=0)
             except:
                 torlog.error("Errored in reverification paused")
             try:
                 client.torrents_file_priority(
-                    torrent_hash=torr, file_ids=resumed, priority=1
-                )
+                    torrent_hash=torr, file_ids=resumed, priority=1)
             except:
                 torlog.error("Errored in reverification resumed")
             client.auth_log_out()
         else:
             break
         k += 1
-        if k >= 2:
+        if k >= 4:
             # avoid an infite loop here
             return False
     return True
@@ -235,9 +657,8 @@ async def re_verfiy(paused, resumed, client, torr):
 @routes.post("/tortk/files/{hash_id}")
 async def set_priority(request):
     torr = request.match_info["hash_id"]
-    client = qba.Client(
-        host="localhost", port="8090", username="admin", password="adminadmin"
-    )
+    client = qba.Client(host="localhost", port="8090",
+                        username="admin", password="adminadmin")
     client.auth_log_in()
 
     data = await request.post()
@@ -260,14 +681,16 @@ async def set_priority(request):
     torlog.info(f"Resumed {resume} of {torr}")
 
     try:
-        client.torrents_file_priority(torrent_hash=torr, file_ids=pause, priority=0)
+        client.torrents_file_priority(
+            torrent_hash=torr, file_ids=pause, priority=0)
     except qba.NotFound404Error:
         raise web.HTTPNotFound()
     except:
         torlog.info("Errored in paused")
 
     try:
-        client.torrents_file_priority(torrent_hash=torr, file_ids=resume, priority=1)
+        client.torrents_file_priority(
+            torrent_hash=torr, file_ids=resume, priority=1)
     except qba.NotFound404Error:
         raise web.HTTPNotFound()
     except:
